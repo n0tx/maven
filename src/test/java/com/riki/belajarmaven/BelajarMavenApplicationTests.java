2da5dbeb16c094bf90eaa36f1317d4e6cd58c76d
n0tx <rcandra91@msn.com>
2023-11-27 01:50:31 +0700

    rest api for maven test

---
+++ src/test/java/com/riki/belajarmaven/BelajarMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riki.belajarmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
